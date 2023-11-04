c549390d9d32c4fddef4c5409c5eca935c60044b
muhammadmesum72 <me.m.mesum@gmail.com>
2023-11-04 17:07:53 +0500

    Header Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import Logo from "../assets/Communi_Teez favicon.jpeg";
 
 const Header = () => {
   return (
     <div className="bg-teal-500">
       <div className="w-full py-6 flex items-center justify-between  container mx-auto text-center md:text-left ">
-        <h1 className="text-xl  md:text-2xl font-semibold w-full">
-        Communi_Teez Design
-        </h1>
+        <div className="text-xl  md:text-2xl font-semibold w-full flex items-center px-3 md:px-0 gap-3">
+          <img src={Logo} alt="" className="w-12" />
+          <span>Communi_Teez Design</span>
+        </div>
       </div>
     </div>
   );
